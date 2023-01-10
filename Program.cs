9fc3b83f50e2c4db3ee1c74ef0a794a7ebb018ac
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-10 19:39:40 +0530

    Greetings

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue_UsingLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to the Stack And Queue Programs(using LinkedList)");
+
+        }
+    }
+}
