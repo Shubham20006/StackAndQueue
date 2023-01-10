6acae9831e42bbf3023e841fa3474910d7812121
Shubham20006 <shubhamchaudhari1506@gmail.com>
2023-01-10 22:05:48 +0530

    Creating Queue using linkedlist

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,31 +11,17 @@ namespace StackAndQueue_UsingLinkedList
     {
         public static void Main(string[] args)
         {
-            Stack obj = new Stack();
+            Queue obj = new Queue();
 
-            // insert Stack value
-            obj.push(70);
-            obj.push(30);
-            obj.push(56);
-
-            // print Stack elements
-            Console.WriteLine("------After push------");
-            obj.Display();
-            Console.WriteLine("\n--------------------------------------------");
-            for (int i = 0; i < 4; i++)
-            {
-                Console.WriteLine("------After pop------");
-                obj.pop();
-                obj.Display();
-                Console.WriteLine("\n--------------------------------------------");
-                Console.WriteLine("------After peek------");
-                obj.peek();
-                Console.WriteLine("\n--------------------------------------------");
-                Console.WriteLine("------Stack Empty : True/False------");
-                Console.WriteLine(obj.isEmpty());
-                Console.WriteLine("\n--------------------------------------------");
-            }
+            // insert Queue value
+            obj.enqueue(56);
+            obj.enqueue(30);
+            obj.enqueue(70);
+            obj.display();
+           
+            
             Console.ReadLine();
+
         }
     }
 }
---
+++ Queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue_UsingLinkedList
+{
+    public class Queue
+    {
+        public Node head, tail;
+        public Queue()
+        {
+            this.head = this.tail = null;
+        }
+        public void enqueue(int data)
+        {
+           Node temp = new Node(data);
+            if (this.tail == null)
+            {
+                this.head = this.tail = temp;
+                return;
+            }
+
+            this.tail.next = temp;
+            this.tail = temp;
+
+        }
+      
+    
+        public void display()
+        {
+            // check for stack underflow
+            if (head == null)
+            {
+                Console.Write("Empty");
+                return;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+
+                    // print node data
+                    Console.Write(temp.data);
+
+                    // assign temp link to temp
+                    temp = temp.next;
+                    if (temp != null)
+                        Console.Write("->");
+                }
+            }
+
+
+        }
+    }        
+}
